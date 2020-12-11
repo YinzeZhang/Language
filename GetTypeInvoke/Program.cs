cb5576c6f294d33415567a808f42ebae049ad8a2
YinzeZhang <1285763952@qq.com>
2020-12-11 22:23:38 +0800

    Create a C# invoke project

---
+++ GetTypeInvoke/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GetTypeInvoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj = new
+            {
+                Name = "小明",
+                Num = 1
+            };
+
+            Console.WriteLine("Name: \t" + obj.GetType().GetProperty("Name").GetValue(obj));
+            Console.WriteLine("Num: \t" + obj.GetType().GetProperty("Num").GetValue(obj));
+        }
+    }
+}
